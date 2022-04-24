df6ad1f93712284d136119a71d47957bfce86ea0
hraayan <hraayan@gmail.com>
2022-04-24 17:44:47 -0500

    add solution w2.e2

--- weekly_mission_2/exercises/exercise2.js
+++ weekly_mission_2/exercises/exercise2.js
@@ -88,3 +88,12 @@ console.log(explorerJs)
 
 const explorerCDMX = explorers.find((explorer) => explorer.city === "CDMX");
 console.log(explorerCDMX)
+
+//Obtén la validación si al menos uno de los explorers tiene la propiedad exercisesFinished en frontend como true, usa SOME
+var somefinished = explorers.some((frontend) => frontend.missions.frontend.exercisesFinished === true)
+console.log("hay almenos uno que haya terminado?: "+ somefinished)
+
+//Obtén la validación si todos los explorers tienen la propiedad isFinished del onboarding como true. Usa EVERY.
+
+var onbFinished = explorers.every((explorer) =>  explorer.missions.onboarding.isFinished === true)
+console.log("Todos terminaron onboarding: "+ onbFinished)

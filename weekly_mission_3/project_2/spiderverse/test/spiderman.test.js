1d9f9a46bf98144385cb2cbdc239684abd7ce263
hraayan <hraayan@gmail.com>
2022-04-26 19:10:57 -0500

    agregando unit tests

--- weekly_mission_3/project_2/spiderverse/test/spiderman.test.js
+++ weekly_mission_3/project_2/spiderverse/test/spiderman.test.js
@@ -1,6 +1,20 @@
-describe("Test Suite Dummy Description", () => {
-    test('Case 1 Dummy', () => {
-      const resultOfSomething = 1 + 2
-      expect(resultOfSomething).toBe(18);
+const Spiderman = require('./../app/spiderman')
+describe("Test spiderman test", () => {
+    test('1) create an spiderman object', () => {
+        const andrewGarField = new Spiderman("Spiderman Sony",31, "Andrew Garfield", 2, "Sony");
+        expect(andrewGarField.name).toBe("Spiderman Sony")
+        expect(andrewGarField.age).toBe(31)
+        expect(andrewGarField.actor).toBe("Andrew Garfield")
+        
+
+
     });
   })
+describe("Unit test for Tom Holland Marvel", () => {
+  test('2) Use the method getInfo()', () =>{
+    const tomHolland = new Spiderman("Spiderman Marvel", 25, "Tom Holland", "Marvel")
+    expect(tomHolland.getInfo()).toBe("Hey, I 'm Tom Holland from Marvel studio")
+
+
+  });
+});

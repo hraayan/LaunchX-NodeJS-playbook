8f1ddb559eb335f4e73ad3b3d07f01107c1ac300
hraayan <hraayan@gmail.com>
2022-05-03 18:44:35 -0500

    creating hhtp api

--- weekly_mission_3/express_basic/app.js
+++ weekly_mission_3/express_basic/app.js
@@ -28,3 +28,29 @@ app.get('/explorers', (req, res) => {
 app.get('/explorers/:explorerName', (req, res) =>{
     res.send(req.params)
 })
+
+//http methods
+
+
+app .get('/v1/explorers', (req,res) =>{
+    console.log(`Api explorers get all request ${new Date()}`)
+    const explorer1 = {id:1, name:"Harry 1"}
+    const explorer2 = {id:2, name:"Harry 2"}
+    const explorer3 = {id:3, name:"Harry 3"}
+    const explorer4 = {id:4, name:"Harry 4"}
+    const explorer5 = {id:5, name:"Harry 5"}
+    const explorers = [explorer1,explorer2,explorer3,explorer4,explorer5]
+    res.status(200).json(explorers)
+})
+//get explorer by id
+app .get('/v1/explorers/:id', (req,res) =>{
+    console.log(`Api explorers get all request ${new Date()}`)
+    console.log(`getting explorer with id: ${req.params.id}`)
+    const explorer1 = {id:1, name:"Harry 1"}
+    const explorer2 = {id:2, name:"Harry 2"}
+    const explorer3 = {id:3, name:"Harry 3"}
+    const explorer4 = {id:4, name:"Harry 4"}
+    const explorer5 = {id:5, name:"Harry 5"}
+    const explorers = [explorer1,explorer2,explorer3,explorer4,explorer5]
+    res.status(200).json(explorer1)
+})
